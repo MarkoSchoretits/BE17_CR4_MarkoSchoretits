e7e759118faf44e364d96dc4650e17485c07197e
MarkoSchoretits <marko@schoretits.com>
2022-11-11 17:49:45 +0100

    DB after FINAL TEST uploaded

--- 00-be17-cr4-markoschoretits_biglibrary.sql
+++ 00-be17-cr4-markoschoretits_biglibrary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Nov 2022 um 17:46
+-- Erstellungszeit: 11. Nov 2022 um 10:13
 -- Server-Version: 10.4.21-MariaDB
 -- PHP-Version: 8.0.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `be17_cr4_markoschoretits_biglibrary`
+-- Datenbank: `be17-cr4-markoschoretits_biglibrary`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `item` (
-  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
   `title` varchar(64) DEFAULT NULL,
   `isbn` char(13) DEFAULT NULL,
   `short_description` text DEFAULT NULL,
@@ -46,9 +46,9 @@ CREATE TABLE `item` (
 -- Daten für Tabelle `item`
 --
 
-INSERT INTO `item` (`id`, `title`, `isbn`, `short_description`, `item_type`, `picture`, `author_first_name`, `author_last_name`, `publisher_name`, `publisher_address`, `publish_date`, `available`) VALUES
-(1, 'Rocky 1', '3140004200001', 'Balboa fights for the 1st time', 'BOOK', 'RB01.png', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1981-01-01', '1'),
-(2, 'Rocky 2', '3140004200002', 'Balboa fights for the 2nd time', 'BOOK', 'RB02.jpeg', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1982-02-02', '1'),
+INSERT INTO `item` (`item_id`, `title`, `isbn`, `short_description`, `item_type`, `picture`, `author_first_name`, `author_last_name`, `publisher_name`, `publisher_address`, `publish_date`, `available`) VALUES
+(1, 'Rocky 1', '3140004200001', 'Balboa fights for the 1st time', 'BOOK', 'RB01.jpg', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1981-01-01', '1'),
+(2, 'Rocky 2', '3140004200002', 'Balboa fights for the 2nd time', 'BOOK', 'RB02.jpg', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1982-02-02', '1'),
 (3, 'Rocky 3', '3140004200003', 'Balboa fights for the 3rd time', 'BOOK', 'RB03.jpg', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1983-03-03', '1'),
 (4, 'Rocky 4', '3140004200004', 'Balboa fights for the 4th time', 'BOOK', 'RB04.jpg', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1984-04-04', '1'),
 (5, 'Rocky 5', '3140004200005', 'Balboa fights for the 5th time', 'BOOK', 'RB05.jpg', 'Adrian', 'Pennino', 'BALBOA INC', 'Philadelphia', '1985-05-05', '1'),
@@ -56,10 +56,7 @@ INSERT INTO `item` (`id`, `title`, `isbn`, `short_description`, `item_type`, `pi
 (7, 'Rocky Encyclopedia 2', '3140004203002', 'Why Rocky fights', 'CD', 'RBE02.jpg', 'Apollo', 'Creed', 'CREED INC', 'Baltimore', '1992-02-02', '1'),
 (8, 'Rocky The Film 1', '3140004207001', 'Balboa fights in 3D', 'DVD', 'RB3D.jpg', 'Ivan', 'Drago', 'DRAGO INC', 'Siberia', '2001-01-01', '1'),
 (9, 'Rocky The Film 2', '3140004207002', 'Balboa fights in 4D', 'DVD', 'RB4D.jpg', 'Ivan', 'Drago', 'DRAGO INC', 'Siberia', '2002-02-02', '1'),
-(10, 'Rocky The Film 3', '3140004207003', 'Balboa fights in 5D', 'DVD', 'RB5D.jpg', 'Ivan', 'Drago', 'DRAGO INC', 'Siberia', '2003-03-03', '1'),
-(11, 'Rambo1', '1234567890001', 'Rambo in Action', 'DVD', '636e3f3761904.jpg', 'Sly', 'Stallone', 'Stallone INC', 'Hollywood', '2011-01-01', '1'),
-(12, 'Rambo 2x', '1234567890017', 'Rambo die zweitex', 'BOOK', '636e58c7ac742.jpg', 'Sylvesterx', 'Stallonex', 'Stallone INCx', 'Hollywoodx', '2012-02-17', '0'),
-(13, 'Rambo 3', '1234567890003', 'Rambo 33/3 - Schnappsklappe!', 'CD', '636e5b0ac7a98.jpg', 'Sly', 'Stallone', 'Stallone INC', 'Hollywood', '2013-03-03', '0');
+(10, 'Rocky The Film 3', '3140004207003', 'Balboa fights in 5D', 'DVD', 'RB5D.jpg', 'Ivan', 'Drago', 'DRAGO INC', 'Siberia', '2003-03-03', '1');
 
 --
 -- Indizes der exportierten Tabellen
@@ -69,7 +66,7 @@ INSERT INTO `item` (`id`, `title`, `isbn`, `short_description`, `item_type`, `pi
 -- Indizes für die Tabelle `item`
 --
 ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`item_id`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -79,7 +76,7 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT für Tabelle `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
